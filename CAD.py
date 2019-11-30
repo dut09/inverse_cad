8f88ed22f723bfc82f17df2866f5927d687e6e41
Kevin Ellis <ellisk@mit.edu>
2019-11-30 13:50:14 -0500

    demo of some possible bugs

--- CAD.py
+++ CAD.py
@@ -16,24 +16,26 @@ class Vertex(Feature):
     def __init__(self,x,y=None,z=None):
         if y is None:
             x,y,z = x.x,x.y,x.z
-        self.p = (x,y,z)
+        self.p = np.array([float(x),float(y),float(z)])
+        # for bizarre reasons we actually need to represent these as strings
+        self.x = str(x)
+        self.y = str(y)
+        self.z = str(z)
     def child(self,o): return False
     def close(self,o,e=0.01):
-        d = sum((u - v)**2
-                for u,v in zip(self.p,o.p) )**0.5
-        return d < e
-    def __hash__(self): return hash(self.p)
+        return ((self.p - o.p)*(self.p - o.p)).sum()**0.5 < e
+    def __hash__(self): return hash(tuple(list(self.p)))
     def __eq__(self,o):
-        return isinstance(o,Vertex) and self.p == o.p
+        return isinstance(o,Vertex) and (self.p == o.p).all()
     def __ne__(self,o):
         return not (self == o)
     def __str__(self):
-        return f"Vertex{self.p}"
+        return f"Vertex({self.x},{self.y},{self.z})"
     def __repr__(self): return str(self)
     def __gt__(self,o):
-        return self.p > o.p
+        return tuple(list(self.p)) > tuple(list(o.p))
     def __lt__(self,o):
-        return self.p < o.p
+        return tuple(list(self.p)) < tuple(list(o.p))
     def __add__(self,o):
         if isinstance(o,Vertex):
             return Vertex(*[A + B for A,B in zip(self.p,o.p) ])
@@ -44,9 +46,6 @@ class Vertex(Feature):
     def __mul__(self,w):
         assert isinstance(w,(float,int))
         return Vertex(*[w*c for c in self.p ])
-
-    def numpy(self):
-        return np.array(self.p)
 
 class Edge(Feature):
     def __init__(self,u,v):
@@ -68,10 +67,7 @@ class Edge(Feature):
     def __repr__(self): return str(self)
 
     def numpy(self):
-        dx = self.e[0].p[0] - self.e[1].p[0]
-        dy = self.e[0].p[1] - self.e[1].p[1]
-        dz = self.e[0].p[2] - self.e[1].p[2]
-        return np.array([dx,dy,dz])
+        return self.e[0].p - self.e[1].p
 
 class Face(Feature):
     def __init__(self, cycles):
@@ -222,7 +218,7 @@ class Extrusion():
         return compilation
 
     def execute(self,c):
-        return c.extrude([v.p for v in self.vertices],
+        return c.extrude([(v.x,v.y,v.z) for v in self.vertices],
                          self.displacement,
                          self.union)
 
@@ -238,85 +234,24 @@ class Extrusion():
         faceID = random.choice(range(c.child.GetSceneHalfFacetNumber()))
         f = c.child.GetSceneHalfFacet(faceID)
         polygon = c.child.GenerateRandomPolygon(faceID, 0.5, 0.5, False)
-        vertices = np.asarray([float(v) for v in polygon.strip().split()]).reshape((-1, 3))
-
-        # figure out the vector which points out of the object
-        v1 = Vertex(c.child.GetSceneVertex(f.cycles[0][0])).numpy()
-        v2 = Vertex(c.child.GetSceneVertex(f.cycles[0][1])).numpy()
-        v3 = Vertex(c.child.GetSceneVertex(f.cycles[0][2])).numpy()
-        normal = np.cross(v2 - v1, v3 - v2)
-        print(f.outward,"facing outward")
-        if not f.outward: normal = -normal        
+        print("created random polygon",polygon)
+        vertices = polygon.strip().split()
+        vertices = [Vertex(vertices[3*i],vertices[3*i + 1],vertices[3*i + 2])
+                    for i in range(len(vertices)//3)]
+        
+        normal = np.asarray([float(v) for v in f.normal.split()])
+        if not f.outward: normal = -normal
         L = ((normal*normal).sum()**0.5)
         if L < 0.001:
             print("for some reason the normal is nothing")
             return Extrusion.sample(c, union=union)
-        
         normal = normal/L
 
         magnitude = random.random()*3 + 1
         direction = list(normal*magnitude)
-        command = Extrusion(direction, union,
-                            [Vertex(*list(v)) for v in vertices])
+        command = Extrusion(direction, union, vertices)
         return command
             
-        if face is None:
-            assert union
-            a = Vertex(-5,-5,0)
-            b = Vertex(5,-5,0)
-            c = Vertex(5,5,0)
-            d = Vertex(-5,5,0)
-            face = Face([frozenset([Edge(a,b),
-                                    Edge(b,c),
-                                    Edge(c,d),
-                                    Edge(d,a)])])
-
-        edges = face.cycles[0]
-        normals = [cp
-                   for _e1 in edges for _e2 in edges
-                   for e1 in [_e1.numpy()]
-                   for e2 in [_e2.numpy()] 
-                   for cp in [np.cross(e1,e2)]
-                   if (cp*cp).sum()/((e1*e1).sum()*(e2*e2).sum()) > 0.1]
-        normal = random.choice(normals)
-        normal = normal/((normal*normal).sum()**0.5)
-
-
-        vs = list({v for e in face.cycles[0] for v in e.e })
-        newVertices = []
-        for _ in range(0,random.choice(range(3,8))):
-            W = np.random.dirichlet([1]*len(vs))
-            V = sum(v*w for v,w in zip(vs,W) )
-            newVertices.append(np.array(V.p))
-        # add a dummy vertex to make them not all be coplanar
-        newVertices.append(newVertices[-1] + normal)
-        
-        H = scipy.spatial.ConvexHull(np.array(newVertices))
-        import pdb; pdb.set_trace()
-        
-        
-        
-        
-        
-        
-        if union:
-            angle = 0
-            N = random.choice(range(3,8))
-            r = random.random() + 1
-            face = [Vertex(r,r,0)]
-            for _ in range(N - 1):            
-                d_angle = 2*math.pi/N#6.28/N
-
-                x,y,_ = face[-1].p
-                x = x + r*math.cos(angle + d_angle)
-                y = y + r*math.sin(angle + d_angle)
-
-                angle += d_angle
-
-                face.append(Vertex(x,y,0))
-            return Extrusion((0,0,1),True,face)
-        else:
-            assert False
 
 class Program():
     def __init__(self, commands):
--- test.py
+++ test.py
@@ -4,11 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 
+import sys
+if len(sys.argv) != 2 or sys.argv[1] not in {'0','1'}:
+    print("Usage: python test.py [0|1]")
+    sys.exit(0)
+
 # create a 3-D figure to illustrate the faces
 fig = plt.figure()
 ax = p3.Axes3D(fig)
 
+COLORS = ['r','b','g','orange']
 def showExtrusion(vertices, displacement):
+    global COLORS
+    
     vertices = vertices.split(" ")
     # pack up into an array
     vertices = [ [float(vertices[3*i]),float(vertices[3*i+1]),float(vertices[3*i+2])]
@@ -16,27 +24,36 @@ def showExtrusion(vertices, displacement):
     vertices.append(vertices[0])
 
     vertices = np.array(vertices)
-    ax.plot(vertices[:,0],vertices[:,1],vertices[:,2])
+    ax.plot(vertices[:,0],vertices[:,1],vertices[:,2],
+            color=COLORS[0])
 
     # also illustrate what would happen if you extruded the face along this vector
-    d = np.array(displacement)
+    d = np.array(list(map(float,displacement.split())))
     vertices = vertices + d
-    ax.plot(vertices[:,0],vertices[:,1],vertices[:,2])
+    ax.plot(vertices[:,0],vertices[:,1],vertices[:,2],
+            color=COLORS[0])
 
+    COLORS = COLORS[1:]
 
+commands = {'0': ["5 0 0 5 0 2.68747 5 1.74934 3 5 5 3 3.6042687739429273 0.0 0.0",
+                  "5 0 0 5 4.47911 2.68747 6.26102 5 3 8.60427 5 3 -0.0 1.669884959000657 -2.7831364504696787"],
+              '1': ["0 5 3 5 5 3 5 5 0.895277 3.59781 5 0 0 5 0 0.0 -3.534620585254398 0.0",
+              "5 1.46538 3 5 5 3 1.49213 5 3 0 4.00876 3 0 1.46538 3 0.0 0.0 3.8627544028619845"]}[sys.argv[1]]
 
-showExtrusion("1.36496 1.36496 3.10004 1.36496 1.36496 1.03412 1.44766 0.992338 0.846285 1.66791 0.0 3.10004",
-              [1.4520674377395917, 0.3222833125243299, -0.0])
+print("showing extrusions, color ordering is: ",COLORS)
+for k in commands:
+    print(k)
+    polygon = " ".join(k.split()[:-3])
+    vector = " ".join(k.split(" ")[-3:])
+    showExtrusion(polygon, vector)
 
-showExtrusion("5.0 0.0 0.0 1.66791 0.0 0.0 1.36496 1.36496 0.0 5.0 5.0 0.0",
-              [-0.0, -0.0, 3.1000430377011106])
 plt.show()
 
 # now try to build the thing using the Python APIs
 c = Scene()
 
-
-c.ExtrudeFromString("extrude 5.0 0.0 0.0 1.66791 0.0 0.0 1.36496 1.36496 0.0 5.0 5.0 0.0 -0.0 -0.0 3.1000430377011106 +")
-c.ExtrudeFromString("extrude 1.36496 1.36496 3.10004 1.36496 1.36496 1.03412 1.44766 0.992338 0.846285 1.66791 0.0 3.10004 1.4520674377395917 0.3222833125243299 -0.0 +")
+for k in commands:
+    print("about to execute",k)
+    c.ExtrudeFromString(f"extrude {k} +")
 
 c.SaveScene("/tmp/BUG.off")
